565e0af79423036cf6c067fba131a43be2167074
chollingsworth <caleb.hollingsworth10@gmail.com>
2021-04-16 16:44:02 -0500

    initial styling complete

--- src/Modal/SetTime.js
+++ src/Modal/SetTime.js
@@ -15,34 +15,37 @@ const SetTime = (props) => {
 	return (
 		<div className='set-time-container'>
 			<form
+				className='set-time-form'
 				onSubmit={(e) => {
 					timeChangeHandler(e);
 				}}>
-				<div>
-					Hours
-					<input
-						type='number'
-						name='hours'
-						onChange={(e) => setHours(e.target.value * 3600)}
-					/>
+				<div className='set-time-inputs'>
+					<div className='set-time-hours'>
+						Hours
+						<input
+							type='number'
+							name='hours'
+							onChange={(e) => setHours(e.target.value * 3600)}
+						/>
+					</div>
+					<div className='set-time-minutes'>
+						Minutes
+						<input
+							type='number'
+							name='minutes'
+							onChange={(e) => setMinutes(e.target.value * 60)}
+						/>
+					</div>
+					<div className='set-time-seconds'>
+						Seconds
+						<input
+							type='number'
+							name='seconds'
+							onChange={(e) => setSeconds(e.target.value * 1)}
+						/>
+					</div>
 				</div>
-				<div>
-					Minutes
-					<input
-						type='number'
-						name='minutes'
-						onChange={(e) => setMinutes(e.target.value * 60)}
-					/>
-				</div>
-				<div>
-					Seconds
-					<input
-						type='number'
-						name='seconds'
-						onChange={(e) => setSeconds(e.target.value * 1)}
-					/>
-				</div>
-				<div>
+				<div className='set-time-submit'>
 					<input
 						type='submit'
 						onSubmit={(e) => {
